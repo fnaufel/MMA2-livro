1346f2131b992436336d2c8eb682b235acea7dc9
Fernando Naufel <fnaufel@gmail.com>
2023-11-21 18:36:14 -0300

    Continue ch 1

---
+++ capture001.R
@@ -0,0 +1,36 @@
+
+# 001 ---------------------------------------------------------------------
+
+source('webshot.R')
+
+slides <- c(
+  '2',
+  '3/1/4',
+  '4',
+  '5/1/8',
+  '6/1/4',
+  '7/1/7',
+  '8/1/0',
+  '9',
+  '9/1/0',
+  '9/1/1',
+  '9/1/2',
+  '9/1/3',
+  '9/1/4',
+  '9/1/5',
+  '9/1/6',
+  '10/1/3',
+  '11/1/2',
+  'references'
+)
+
+slides <- paste0(
+  'file:///home/fnaufel/Documents/UFF/Ensino/Disciplinas/MMA/MMA2-livro/slides/10-primeiras-nocoes/slides-001.html#/',
+  slides
+)
+
+capture(
+  slides,
+  '/home/fnaufel/Documents/UFF/Ensino/Disciplinas/MMA/MMA2-livro/slides/10-primeiras-nocoes/captured',
+  delay = 5
+)
